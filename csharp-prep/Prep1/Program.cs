c5ded7f7171aa8e1001e342c0053717df6fc39a0
Benjamin G Barnes <bar22088@byui.edu>
2024-04-27 18:46:48 -0600

    It works. Huzzah!

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What's your name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("And your Last name? ");
+        string surname = Console.ReadLine();
+
+        Console.Write("What's your middle name? At least give a middle initail. ");
+        string middle = Console.ReadLine();
+
+        Console.Write("Any preferred names or titles? ");
+        string nickname = Console.ReadLine();
+
+        Console.WriteLine($"So you full name is {first} {middle} {surname}, We'll just stick with {nickname}. ");
     }
 }
