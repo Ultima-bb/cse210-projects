4ad4b02baae8ee40d429398d9c0e1c5036a27546
Benjamin G Barnes <bar22088@byui.edu>
2024-04-27 19:59:13 -0600

    Well. It's repetative. But it works.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,64 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Are you passing this class? What's your current grade? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string grade = "";
+
+        if (percent == 100)
+        {grade = "A+";}
+
+        else if (percent >= 93)
+        {grade = "A";}
+
+        else if(percent >= 90)
+        {grade = "A-";}
+
+        else if(percent >= 86)
+        {grade = "B+";}
+
+        else if(percent >= 82)
+        {grade = "B";}
+
+        else if(percent >= 80)
+        {grade = "B-";}
+
+        else if(percent >= 76)
+        {grade = "C+";}
+
+        else if(percent >= 72)
+        {grade = "C";}
+
+        else if(percent >= 70)
+        {grade = "C-";}
+
+        else if(percent >= 66)
+        {grade = "D+";}
+
+        else if(percent >= 62)
+        {grade = "D";}
+
+        else if(percent >= 60)
+        {grade = "D-";}
+
+        else
+        {grade = "F";}
+
+        Console.WriteLine($"So your grade is a {grade} ");
+
+        if (percent >= 70)
+        {Console.WriteLine("That works. You'll pass no problem.");}
+
+        else
+        {Console.WriteLine("Wow. Um... You're gonna have to retake the class buddy.");}
+
+
+
     }
 }
