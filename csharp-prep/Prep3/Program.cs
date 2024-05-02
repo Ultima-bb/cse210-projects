e9d233e1af1c26a6759b9b53001d6f38ddf03083
Benjamin G Barnes <bar22088@byui.edu>
2024-05-02 15:11:14 -0600

    I ended up using the example to make it work...

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int Number = randomGenerator.Next(1, 1000);
+        int guess =-1;
+        
+        while (guess != Number)
+        {   Console.Write("And the Magic Number from 1 to 1000 is? ");
+            guess = int.Parse(Console.ReadLine());
+        
+            if (Number > guess)
+            {Console.WriteLine("Greater than that." );}
+
+            else if (Number < guess)
+            {Console.WriteLine("The Numer's lower than that.");}
+
+            else
+            {Console.WriteLine("Well, well. Threading the needle aren't we? You're right, good guess.");}
+
+
+        }
     }
 }
