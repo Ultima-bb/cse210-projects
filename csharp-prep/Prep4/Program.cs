1011571a255c9c0c105e991a28a536e418a1fb9b
Benjamin G Barnes <bar22088@byui.edu>
2024-05-03 18:53:54 -0600

    How did I forget the Float? Remember next Time BEN!!!

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
+        {List<int> numbers = new List<int>();
+        int input = -1;
+        while (input != 0)
     {
-        Console.WriteLine("Hello Prep4 World!");
+            Console.Write("Number? (enter 0 to quit)");
+            string userResponse = Console.ReadLine();
+            input = int.Parse(userResponse);
+            if (input != 0)
+                {numbers.Add(input);}
+    }
+    
+        int Total = 0;
+        foreach (int number in numbers)
+            {Total+= number;}
+
+        Console.WriteLine($"The total is {Total}.");
+        float average = ((float)Total)/numbers.Count;
+        Console.WriteLine($"The average is {average}");
+        int Highest = numbers[0];
+        foreach (int number in numbers)
+            {if (number>Highest)
+                {Highest=number;}
+}
+    Console.WriteLine($"The highest number is {Highest}");
     }
 }
