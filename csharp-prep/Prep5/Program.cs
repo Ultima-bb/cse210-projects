5956b39d07f3a78f166a94a38d0ffeb6b33096c6
Benjamin G Barnes <bar22088@byui.edu>
2024-05-03 19:36:51 -0600

    I made some weird mistakes on this one.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -3,7 +3,30 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
+    {   Message();
+        string GivenName = PromptUserName();
+        int FavoriteNum = PromptUserNumber();
+        int FavTimeFav = SquareNumber(FavoriteNum);
+        DisplayResult(GivenName,FavTimeFav);
+        
     }
+    static void Message()
+    {Console.WriteLine("Welcome to the program. ");}
+
+    static string PromptUserName()
+    {Console.Write("What's your name? ");
+    string name = Console.ReadLine();
+    return name;}
+
+    static int PromptUserNumber()
+    {Console.Write("What's you favorite number? ");
+    int number = int.Parse(Console.ReadLine());
+    return number;}
+
+    static int SquareNumber (int number)
+    {int square = number * number;
+    return square;}
+
+    static void DisplayResult(String name, int square)
+    {Console.WriteLine($"So you're name is {name}, and your favorite number squared is {square}.");}
 }
