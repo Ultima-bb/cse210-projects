a90e5e1c1781f92c818889597aab60fd7b7fafa7
Benjamin G Barnes <bar22088@byui.edu>
2024-05-10 22:17:50 -0600

    When I get this without help, it shall be glorious.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Newspaper Courrier";
+        job1._company = "Niagara Falls Gazette";
+        job1._startYear = 2002;
+        job1._endYear = 2009;
+    
+    
+        Job job2 = new Job();
+        job2._jobTitle = "Production Worker";
+        job2._company = "Ford Motor Company";
+        job2._startYear = 2019;
+        job2._endYear = 2022;
+
+        Job job3 = new Job();
+        job3._jobTitle = "Deli Worker";
+        job3._company = "Top Friendly Markets";
+        job3._startYear = 2023;
+        job3._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Benjamin G. Barnes";
+
+        myResume._jobs.Add(job2);
+        myResume._jobs.Add(job3);
+        myResume._jobs.Add(job3);
+
+        myResume.Display();
     }
 }
