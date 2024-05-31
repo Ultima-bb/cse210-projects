e582fb48366421a369bf56f567e58f3a3be77559
Benjamin G Barnes <bar22088@byui.edu>
2024-05-30 23:35:39 -0600

    Please press enter multiple times....

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -2,8 +2,8 @@ public class ScriptureReference
 {
     public string Book { get; private set; }
     public int Chapter { get; private set; }
-    public int StartVerse { get; private set; }
-    public int EndVerse { get; private set; }
+    public int Start { get; private set; }
+    public int End { get; private set; }
 
     public ScriptureReference(string reference)
     {
@@ -16,8 +16,8 @@ public class ScriptureReference
         Book = parts[0];
         string[] chapterVerse = parts[1].Split('-');
         Chapter = int.Parse(chapterVerse[0]);
-        StartVerse = int.Parse(chapterVerse[1]);
+        Start = int.Parse(chapterVerse[1]);
         if (chapterVerse.Length == 1)
-            EndVerse = int.Parse(chapterVerse[1]);
+            End = int.Parse(chapterVerse[1]);
     }
 }
